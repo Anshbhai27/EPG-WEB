c36bb4cfe25ae6bb07e4c20905b224f4e6ce51c4
Anshbhai27 <110285045+Anshbhai27@users.noreply.github.com>
2024-12-12 12:42:03 +0530

    Create global.css

---
+++ src/app/styles/global.css
@@ -0,0 +1,17 @@
+@tailwind base;
+@tailwind components;
+@tailwind utilities;
+
+.channel-card {
+  transition: transform 0.2s;
+  cursor: pointer;
+}
+
+.channel-card:hover {
+  transform: scale(1.05);
+}
+
+.show-card {
+  background: white;
+  box-shadow: 0 2px 4px rgba(0,0,0,0.1);
+}
