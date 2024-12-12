c8d4e0ed5e0ba68a3a205d7ad4fc60a28bd217a7
Anshbhai27 <110285045+Anshbhai27@users.noreply.github.com>
2024-12-12 12:47:14 +0530

    Update channels.js

--- src/app/data/channels.js
+++ src/app/data/channels.js
@@ -8,6 +8,5 @@ export const channels = [
     id: 433,
     channel_name: "Nick",
     channel_logo: "https://ltsk-cdn.s3.eu-west-1.amazonaws.com/jumpstart/Temp_Live/cdn/HLS/Channel/imageContent-11658-j9k5lvgo-v1/imageContent-11658-j9k5lvgo-m1.png"
-  },
-  // Add other kids channels here
-]
+  }
+];
