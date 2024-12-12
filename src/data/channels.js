e501e876465c4a25847ba7b30e01ce2bfe36c0ef
Anshbhai27 <110285045+Anshbhai27@users.noreply.github.com>
2024-12-12 12:56:16 +0530

    Create channels.js

---
+++ src/data/channels.js
@@ -0,0 +1,13 @@
+export const channels = [
+  {
+    id: 114,
+    channel_name: "Disney Channel",
+    channel_logo: "https://ltsk-cdn.s3.eu-west-1.amazonaws.com/jumpstart/Temp_Live/cdn/HLS/Channel/imageContent-1356-j5tdnwmw-v1/imageContent-1356-j5tdnwmw-m1.png"
+  },
+  {
+    id: 433,
+    channel_name: "Nick",
+    channel_logo: "https://ltsk-cdn.s3.eu-west-1.amazonaws.com/jumpstart/Temp_Live/cdn/HLS/Channel/imageContent-11658-j9k5lvgo-v1/imageContent-11658-j9k5lvgo-m1.png"
+  }
+  // Add more channels as needed
+];
